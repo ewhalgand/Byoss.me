3c8de43003902c0b98534260ed8e71b49e25bd9b
Proxyfil <piloupilou200@gmail.com>
2022-10-30 13:23:23 +0100

    feat : select menu implemented

--- src/app/Pages/profile/edit-profile/EditProfile.jsx
+++ src/app/Pages/profile/edit-profile/EditProfile.jsx
@@ -1,6 +1,37 @@
 import styles from "./styles/editprofile.module.scss";
+import CreatableSelect from 'react-select/creatable';
 
 const EditProfile = () => {
+
+    let activities = [
+        {value: 'propriétaire', label:'Propriétaire / Fondateur'},
+        {value: 'graphiste', label: 'Graphiste'},
+        {value: 'monteur', label: 'Monteur / Motion Designer'},
+        {value: 'modérateur', label: 'Modérateur'},
+        {value: 'développeur', label: 'Développeur'},
+        {value: 'musicien', label: 'Musicien / Compositeur'},
+        {value: 'youtubeur', label: 'Youtubeur / Vidéaste'},
+        {value: 'web_designer', label: 'Web Designer'}
+    ]
+      
+      const colourStyles = {
+        control: (styles) => ({ ...styles, backgroundColor: 'white' }),
+        option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+          return {
+            ...styles,
+            backgroundColor: '#ffffff00',
+      
+            ':active': {
+              ...styles[':active'],
+              backgroundColor: isDisabled
+            },
+          };
+        },
+        input: (styles) => ({ ...styles, ...{ color: 'black' } }),
+        placeholder: (styles) => ({ ...styles}),
+        singleValue: (styles, { data }) => ({ ...styles}),
+      };
+
     return (
         <main className={styles.main}>
             <img className={styles.title} src="/img/home/title.svg" alt="titre" />
@@ -13,21 +44,38 @@ const EditProfile = () => {
                 </section>
                 <div className={styles.infos}>
                     <input className={styles.input} type="text" placeholder="Votre pseudo" />
-                    <input className={styles.input} type="text" placeholder="Votre email (Facultatif)" />
+                    <input className={styles.input} type="email" placeholder="Votre email (Facultatif)" />
                 </div>
             </section>
             <section className={styles.socials}>
-                <div className={styles.socialsLeft}>
-                    <input className={styles.input} type="text" placeholder="Votre email (Facultatif)" />
-                    <input className={styles.input} type="text" placeholder="Votre email (Facultatif)" />
-                    <input className={styles.input} type="text" placeholder="Votre email (Facultatif)" />
+                <div>
+                    <input className={styles.input} type="text" placeholder="pseudo Discord (Facultatif)" />
+                    <input className={styles.input} type="text" placeholder="pseudo Youtube (Facultatif)" />
                 </div>
-                <div className={styles.socialsRight}>
-                    <input className={styles.input} type="text" placeholder="Votre email (Facultatif)" />
-                    <input className={styles.input} type="text" placeholder="Votre email (Facultatif)" />
-                    <input className={styles.input} type="text" placeholder="Votre email (Facultatif)" />
+                <div>
+                    <input className={styles.input} type="text" placeholder="pseudo Instagram (Facultatif)" />
+                    <input className={styles.input} type="text" placeholder="pseudo Github (Facultatif)" />
+                </div>
+                <div>
+                    <input className={styles.input} type="text" placeholder="pseudo Twitter (Facultatif)" />
+                    <input className={styles.input} type="url" placeholder="Site Web (Facultatif)" />
                 </div>
             </section> 
+            <section className={styles.more}>
+                <div>
+                    <img className={styles.flameIcon}src="/img/profile/edit/flame.svg" /><input className={styles.input} type="text" placeholder="Jeu ou communauté préférée (Facultatif)" />
+                </div>
+                <div>
+                    <textarea rows="5" cols="100" className={styles.inputArea} type="textfield" placeholder="Description du profil" />
+                </div>
+            </section>
+
+            <h2 className={styles.subtitle}>VOS ACTIVITÉS</h2>
+
+            <section className={styles.activities}>
+                <h2 className={styles.activitiesSubtitle}>Ajouter / Retirer des activités</h2>
+                <CreatableSelect styles={colourStyles} className={styles.selectMenu} isMulti options={activities} placeholder='Selectionner des activités'/>
+            </section>
         </main>
     )
 }
